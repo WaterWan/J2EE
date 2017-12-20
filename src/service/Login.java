c8b64f110741054f2bca501e999a114c1bcb8359
WaterWan <598127577@qq.com>
2017-12-20 20:20:48 +0800

    配置完了jndi。虽然有很多瑕疵，但是还是有救的

--- src/service/Login.java
+++ src/service/Login.java
@@ -1,6 +1,11 @@
 package service;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -9,22 +14,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
 
 public class Login extends HttpServlet {
+	private PreparedStatement pst;
+
 	@Override
 	public void init() throws ServletException {
-		// TODO JNDI连接数据库
-		try {
-			Context ctx = new InitialContext();
-			String dataSourceJNDIName = "";
-			DataSource ods = (DataSource) ctx.lookup(dataSourceJNDIName);
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Get.............");
@@ -34,12 +34,38 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("Post...........");
 		String username = req.getParameter("username");
-		if("".equals(username)) {
+		if ("".equals(username)) {
 			throw new ServletException("Username is empty!");
 		}
 		String password = req.getParameter("password");
-		if("".equals(password)) {
+		if ("".equals(password)) {
 			throw new ServletException("Password is empty!");
 		}
+		PrintWriter writer = resp.getWriter();
+		// TODO JNDI连接数据库
+		try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/j2ee_shwk2");
+            Connection conn = ds.getConnection();
+			System.out.println("--------数据库连接成功！！！----------");
+			String sql = "select * from users where username = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, username);
+			ResultSet resultSet = pst.executeQuery();
+			if(resultSet.next()) {
+				if(!resultSet.getString(2).equals(password)) {
+					throw new ServletException("Wrong password!");
+				} else {
+					// TODO: 登录成功
+				}
+			} else {
+				throw new ServletException("No such user!");
+			}
+		} catch (SQLException | NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
 }
